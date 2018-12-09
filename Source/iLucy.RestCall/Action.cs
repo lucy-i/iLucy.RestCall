6b4e153db79639f9a546cd9bf383d3fa4c4656a3
lucy-i <lucy@ilucy.in>
2018-12-09 10:24:39 -0800

    Added Action Calss

---
+++ Source/iLucy.RestCall/Action.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+
+namespace iLucy.RestCall
+{
+    public interface IResponse<TResult> 
+    {
+        HttpResponseMessage Response { get; set; }
+        TResult Result { get; set; }
+    }
+
+    internal interface IAction
+    {
+        IResponse<object> Get();
+        IResponse<object> Get(string[] urlParams);
+        IResponse<object> Post(HttpContent content);
+        IResponse<object> Post(HttpContent content, string[] urlParams);
+    }
+
+    internal interface IAction<TResult>
+    {
+        IResponse<TResult> Get();
+        IResponse<TResult> Get(string[] urlParams);
+        IResponse<TResult> Post(HttpContent content);
+        IResponse<TResult> Post(HttpContent content, string[] urlParams);
+    }
+
+    internal interface IAction<TResult, TQModel>: IAction<TResult> 
+    {
+        IResponse<TResult> Get(TQModel queryParam);
+        IResponse<TResult> Post(TQModel queryParam, HttpContent content);
+    }
+
+    public class Action
+    {
+        private HttpClient cli;
+        public void get()
+        {
+            //cli.GetAsync()
+        }
+    }
+
+    public class Action<Model>
+    {
+    }
+
+}
